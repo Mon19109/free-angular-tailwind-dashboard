e6a9de22bfac6cb64a3da57fa948c686e16548a3
Tapia Jacott <tapiajacott2100@gmail.com>
2026-06-10 01:53:49 -0600

    se colocan estilos a tarjeta (checar servicio.)

--- src/app/shared/layout/app-header/app-header.component.ts
+++ src/app/shared/layout/app-header/app-header.component.ts
@@ -22,10 +22,6 @@ import { filter } from 'rxjs/operators';
   templateUrl: './app-header.component.html',
 })
 
-
-
-
-
 export class AppHeaderComponent {
 
 
@@ -36,6 +32,23 @@ export class AppHeaderComponent {
 
 
 tituloPagina = '';
+private readonly titulosPaginas: Record<string, string> = {
+  dashboard: 'Panel Principal',
+  saldos: 'Detalle de saldo',
+  usuarios: 'Usuarios',
+  transacciones_adquirencia: 'Transacciones',
+  operaciones_adquirencia: 'Operaciones',
+  transacciones_emision: 'Transacciones',
+  operaciones_emision: 'Operaciones',
+  tarjeta: 'Tarjeta',
+  pago_distancia: 'Pago a Distancia',
+
+  // futuros módulos
+  reportes: 'Reportes',
+  conciliacion: 'Conciliación',
+  comercios: 'Comercios',
+  cuentas: 'Cuentas'
+};
 
 constructor(
   public sidebarService: SidebarService,
@@ -48,50 +61,21 @@ constructor(
     .pipe(
       filter(event => event instanceof NavigationEnd)
     )
-    .subscribe(() => {
+   .subscribe(() => {
 
-      const url = this.router.url;
+  const url = this.router.url;
 
-      if (url.includes('dashboard')) {
-        this.tituloPagina = 'Panel Principal';
-      }
-      else if (url.includes('saldos')) {
-        this.tituloPagina = 'Detalle de saldo';
-      }
-      else if (url.includes('usuarios')) {
-        this.tituloPagina = 'Usuarios';
-      }
-      else if (url.includes('transacciones_adquirencia')) {
-  this.tituloPagina = 'Transacciones';
+  const rutaEncontrada =
+    Object.keys(this.titulosPaginas)
+      .find(ruta => url.includes(ruta));
+
+  this.tituloPagina =
+    rutaEncontrada
+      ? this.titulosPaginas[rutaEncontrada]
+      : '';
+
+});
 }
-else if (url.includes('operaciones_adquirencia')) {
-  this.tituloPagina = 'Operaciones';
-}
-else if (url.includes('transacciones_emision')) {
-  this.tituloPagina = 'Transacciones Emisión';
-}
-
-else if (url.includes('operaciones_emision')) {
-  this.tituloPagina = 'Operaciones Emisión';
-}
-
-
-
-      else {
-        this.tituloPagina = '';
-      }
-      
-
-    });
-}
-
-
-
-
-
-
-
-
 
 
   handleToggle() {
