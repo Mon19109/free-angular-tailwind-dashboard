6d567a778a4305c37cbf2cbd8edee9cf99de3880
Tapia Jacott <tapiajacott2100@gmail.com>
2026-09-15 15:15:32 -0600

    se cambia el v2 a v3

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   api: {
-    kashpay: '/api/v2/antares.kwt-v2.2.10/',
+    kashpay: '/api/v3/antares.kwt-v2.2.10/',
     linkpago: '/OrderReceiver/api/v1/',
     saldos: '/Entities/entities/',
     auth: '/OAuthServices/',
