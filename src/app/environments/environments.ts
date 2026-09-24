e42c921dbcad9e67807af3df7ad14d2ef4f9da8c
Mon19109 <mony.a.910915@gmail.com>
2026-09-24 12:15:14 -0600

    ajuste a url
    se ajusta la url de servicios de tarjeta para el link de pago

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,16 +3,17 @@ export const environment = {
   production: true,
   api: {
    
-     //Prod
-   kashpay: '/api/v3/antares.kwt-v2.2.10/',
-        //SDBX
-       // kashpay: '/api/v2/antares.kwt-v2.2.10/',
+  //Prod
+    kashpay: '/api/v3/antares.kwt-v2.2.10/',
+  //SDBX
+    //kashpay: '/api/v2/antares.kwt-v2.2.10/',
     linkpago: '/OrderReceiver/api/v1/',
     saldos: '/Entities/entities/',
     auth: '/OAuthServices/',
     entities: '/EntitiesServices/',
     aldebaran: '/kwt-a7f2-v1.3.2/',
     voucher: '/KashPay/v2/',
+    card: '/CardServices/',
     BEARER_TOKEN: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWNzYV90ZWNoX2dlbmVyaWMxQGdtYWlsLmNvbSIsImV4cCI6MTY4NzY1OTU1OSwiaWF0IjoxNjg3NjUzNTgzfQ.PlRXl1AHcQegU2TcrSi_C8bbHs19yjaDaBQ3eZpXiq7ROpERnVVgRp0XNhjqZ-dXLv26-Rkkv1RHBEt71kv-cg',
     documents: '/DocumentsServices/',
     KashpayCoreAPI: '/KashpayCoreAPI/api/v1/',
